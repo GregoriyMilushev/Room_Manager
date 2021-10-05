c06bd628aa8d45e98dc2df2656f864730816094f
Gregoriy Milushev <burzakata@gmail.com>
2021-10-05 13:29:56 +0300

    Desk test added

--- tests/Unit/DeskTest.php
+++ tests/Unit/DeskTest.php
@@ -3,12 +3,15 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use App\Models\Room;
+use App\Models\Desk;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class DeskTest extends TestCase
 {
-    // use RefreshDatabase;
+    use RefreshDatabase;
 
     /**
      * A basic unit test example.
@@ -17,7 +20,12 @@ class DeskTest extends TestCase
      */
     public function test_admin_getting_all_desks()
     {
-        $user = User::find(1);
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => '12345678',
+            'role' => 'admin'
+        ]);
 
         $this->actingAs($user);
 
@@ -29,9 +37,13 @@ class DeskTest extends TestCase
 
     public function test_admin_create_desk()
     {
-        $user = User::find(1);
+        $user = User::factory()->create([
+            'role' => 'admin'
+        ]);
 
         $this->actingAs($user);
+
+        $room = Room::factory()->create();
 
         $response = $this->post('api/desks',[
             'price_per_week' => 15.50,
@@ -41,5 +53,51 @@ class DeskTest extends TestCase
         ]);
 
         $response->assertStatus(201);
+    }
+
+    public function test_cant_create_different_than_small_or_big_desk()
+    {
+        $user = User::factory()->create([
+            'role' => 'admin'
+        ]);
+
+        $this->actingAs($user);
+
+        $room = Room::factory()->create();
+
+        $response = $this->post('api/desks',[
+            'price_per_week' => 15.50,
+            'position' => 'centered',
+            'size' => 'large',// Large - size
+            'room_id' => 1,
+        ]);
+
+        $this->assertCount(0, Desk::all());
+    }
+
+    public function test_full_room_cant_create_desk()
+    {
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => '12345678',
+            'role' => 'admin'
+        ]);
+        $this->actingAs($user);
+
+        $room = Room::factory()->create([
+            'desk_capacity' => 1
+        ]);
+
+        for ($i=0; $i <= 1; $i++) { 
+            $response = $this->post('api/desks',[
+                'price_per_week' => 15.50,
+                'position' => 'centered',
+                'size' => 'small',
+                'room_id' => 1,
+            ]); 
+        }
+
+        $response->assertStatus(403);
     }
 }
