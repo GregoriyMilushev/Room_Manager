8245373739c4dcc314820dffbdc24ac58e672683
Gregoriy Milushev <burzakata@gmail.com>
2021-10-08 13:52:13 +0300

    Desk Test

--- tests/Unit/DeskTest.php
+++ tests/Unit/DeskTest.php
@@ -12,23 +12,29 @@ use Illuminate\Foundation\Testing\WithFaker;
 
 class DeskTest extends TestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
+  
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * Set up the test
      */
+    // public function setUp(): void
+    // {
+    //     parent::setUp();
+    //     $this->admin =  User::factory()->create([
+    //         'role' => 'admin'
+    //     ]);
+
+    //     $this->manager = User::factory()->create([
+    //         'role' => 'room manager',
+    //     ]);
+    // }
+    
     public function test_admin_getting_all_desks()
     {
-        $user = User::create([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => '12345678',
-            'role' => 'admin'
-        ]);
+        // dd($this->admin);
 
-        $this->actingAs($user);
+        $this->actingAs($this->admin);
 
         $response = $this->get('api/desks');
 
@@ -38,22 +44,23 @@ class DeskTest extends TestCase
 
     public function test_room_manager_can_only_sees_his_room_desks()
     {
+        $this->withoutExceptionHandling();
+
         $manager = User::factory()->create([
             'role' => 'room manager',
         ]);
 
-        $this->actingAs($manager);
+        $this->actingAs($this->manager);
 
-        $room = Room::factory()->create([
-            'manager_id' => 1,
-        ]);
+        $room = Room::factory()->create();
 
         $desks = Desk::factory(3)->create([
             'room_id' => 1,
         ]);
 
-        $response = $this->getJson('api/desks');
+        $response = $this->get('api/desks');
         $response->assertStatus(200);
+        $this->assertCount(3,$room->desks);
         //$response->assertJson(['room_id' => '1']);
     }
 
@@ -67,19 +74,21 @@ class DeskTest extends TestCase
 
         $this->actingAs($client);
 
-        $room = Room::factory()->create();
+        $room = Room::factory()->create([
+            'manager_id' => 2
+        ]);
 
-        $desks = Desk::factory()->create([
+        $desk = Desk::factory()->create([
             'is_taken' => true,
             'user_id' => 2,
         ]);
 
         $response = $this->get('api/desks');
         $response->assertStatus(200);
-        //$response->assertJson(["room_id" => "1"]);
+        $this->assertTrue($desk['position'] == $client->desk['position']);
     }    
     
-    public function test_clien_can_only_sees_his_rented_desk()
+    public function test_clien_cant_sees_other_than_rented_desk()
     {
         $manager = User::factory()->create([
             'role' => 'room manager'
@@ -95,7 +104,6 @@ class DeskTest extends TestCase
 
         $response = $this->get('api/desks');
         $response->assertStatus(403);
-        //$response->assertJson(["room_id" => "1"]);
     }      
 
     public function test_admin_create_desk()
@@ -108,14 +116,27 @@ class DeskTest extends TestCase
 
         $room = Room::factory()->create();
 
-        $response = $this->post('api/desks',[
-            'price_per_week' => 15.50,
-            'position' => 'centered',
-            'size' => 'small',
-            'room_id' => 1,
-        ]);
+        $response = $this->post('api/desks',$this->desk_data());
 
         $response->assertStatus(201);
+        $this->assertCount(1,Desk::all());
+    }
+    
+    /** @test */
+    public function test_admin_cant_create_desk_in_uncreated_room()
+    {
+        $user = User::factory()->create([
+            'role' => 'admin'
+        ]);
+
+        $this->actingAs($user);
+
+        $room = Room::factory()->create();
+
+        $response = $this->post('api/desks',array_merge($this->desk_data(), ['room_id' => 2]));
+
+        $response->assertSessionHasErrors('room_id');
+        $this->assertCount(0,Desk::all());
     }
 
     public function test_client_cant_store_desk()
@@ -126,17 +147,13 @@ class DeskTest extends TestCase
 
         $room = Room::factory()->create();
 
-        $response = $this->post('api/desks',[
-            'price_per_week' => 15.50,
-            'position' => 'centered',
-            'size' => 'small',
-            'room_id' => 1,
-        ]);
+        $response = $this->post('api/desks',$this->desk_data());
 
         $response->assertStatus(401);
+        $this->assertCount(0,Desk::all());
     }
 
-    public function test_cant_create_different_than_small_or_big_desk_size()
+    public function test_admin_cant_create_different_than_small_or_big_desk_size()
     {
         $user = User::factory()->create([
             'role' => 'admin'
@@ -146,78 +163,79 @@ class DeskTest extends TestCase
 
         $room = Room::factory()->create();
 
-        $response = $this->post('api/desks',[
-            'price_per_week' => 15.50,
-            'position' => 'centered',
-            'size' => 'large',// Large - size
-            'room_id' => 1,
-        ]);
+        $response = $this->post('api/desks',array_merge($this->desk_data(),['size' => 'large']));
 
+        $response->assertSessionHasErrors('size');
         $this->assertCount(0, Desk::all());
     }
 
     public function test_full_room_cant_store_desk()
     {
-        $user = User::factory()->create([
+        $admin = User::factory()->create([
             'role' => 'admin',
         ]);
 
-        $this->actingAs($user);
+        $this->actingAs($admin);
 
         $room = Room::factory()->create([
             'desk_capacity' => 1
         ]);
 
         for ($i=0; $i <= 1; $i++) { 
-            $response = $this->post('api/desks',[
-                'price_per_week' => 15.50,
-                'position' => 'centered',
-                'size' => 'small',
-                'room_id' => 1,
-            ]); 
+            $response = $this->post('api/desks',$this->desk_data()); 
         }
 
         $response->assertStatus(403);
+        $this->assertCount(1, Desk::all());
     }
 
     public function test_admin_delete_desk()
     {
-        $user = User::factory()->create([
+        $admin = User::factory()->create([
             'role' => 'admin'
         ]);
 
-        $this->actingAs($user);
+        $this->actingAs($admin);
 
         $room = Room::factory()->create();
 
-        $this->post('api/desks',[
-            'price_per_week' => 15.50,
-            'position' => 'centered',
-            'size' => 'small',
-            'room_id' => 1,
-        ]);
+        $this->post('api/desks',$this->desk_data());
 
         $response = $this->delete('api/desks/1');
 
         $response->assertStatus(200);
+        $this->assertCount(0 ,Desk::all());
+    }
+
+    public function test_room_manager_cant_delete_desk()
+    {
+        $manager = User::factory()->create([
+            'role' => 'room manager'
+        ]);
+
+        $this->actingAs($manager);
+
+        $room = Room::factory()->create();
+
+        Desk::factory()->create();
+
+        $response = $this->delete('api/desks/1');
+
+        $response->assertStatus(401);
+        $this->assertCount(1 ,Desk::all());
     }
 
     public function test_admin_updating_desk()
     {
-        $user = User::factory()->create([
+        $admin = User::factory()->create([
             'role' => 'admin'
         ]);
 
-        $this->actingAs($user);
+        $this->actingAs($admin);
 
         $room = Room::factory()->create();
 
-        $this->post('api/desks',[
-            'price_per_week' => 15.50,
-            'position' => 'centered',
-            'size' => 'small',
-            'room_id' => 1,
-        ]);
+        $this->post('api/desks',$this->desk_data());
 
         $response = $this->put('api/desks/1',[
             'position' => 'next to Window',
@@ -225,24 +243,20 @@ class DeskTest extends TestCase
         ]);
 
         $response->assertStatus(200);
+        $response->assertSee('next to Window');
     }
 
     public function test_room_manager_cant_update_desk()
     {
-        $user = User::factory()->create([
+        $manager = User::factory()->create([
             'role' => 'room manager'
         ]);
 
-        $this->actingAs($user);
+        $this->actingAs($manager);
 
         $room = Room::factory()->create();
 
-        $this->post('api/desks',[
-            'price_per_week' => 15.50,
-            'position' => 'centered',
-            'size' => 'small',
-            'room_id' => 1,
-        ]);
+        $this->post('api/desks',$this->desk_data());
 
         $response = $this->put('api/desks/1',[
             'position' => 'next to Window',
@@ -250,5 +264,15 @@ class DeskTest extends TestCase
         ]);
 
         $response->assertStatus(401);
+    }
+
+    private function desk_data()
+    {
+        return [
+            'price_per_week' => 15.50,
+            'position' => 'centered',
+            'size' => 'small',
+            'room_id' => 1,
+        ];
     }
 }
